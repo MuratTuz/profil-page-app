34e82b8d49489fb3d252f90c2baaf363bfba6746
MuratTuz <murattuzlali@gmail.com>
2021-03-25 13:27:28 +0100

    first commit

---
+++ src/components/profil.jsx
@@ -0,0 +1,16 @@
+import { Button, Container, Row, Col } from 'react-bootstrap';
+
+import LeftPanel from './leftPanel';
+import RightPanel from './rightPanel';
+
+
+const Profil = () => {
+    return (
+        <>
+            <LeftPanel />
+            <RightPanel />
+        </>
+    )
+}
+
+export default Profil;
---
+++ src/components/rightPanel.jsx
@@ -0,0 +1,21 @@
+import { Button, Container, Row, Col } from 'react-bootstrap';
+
+
+
+const RightPanel = () => {
+
+}
+
+function ProfessionalExperience() {
+
+}
+
+const Education = () => {
+
+}
+
+const Courses = () => {
+
+}
+
+export default RightPanel;
